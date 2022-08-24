415cabfdb3d52a531dcadd4200419a8bc5b2bac1
vytautasdev <vytautasdev@gmail.com>
2022-08-24 23:03:18 +0100

    client-first-commit

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'Vinder Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (res) => {
+        this.users = res;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+}
