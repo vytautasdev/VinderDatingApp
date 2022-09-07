b6abdf7c4ab0b1b36b79426d1a9e437001c90922
Vytautas <vytautasdev@gmail.com>
2022-09-07 22:32:42 +0100

    like-user-feature-commit

---
+++ API/Data/LikesRepository.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Helpers;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class LikesRepository : ILikesRepository
+
+
+{
+    private readonly DataContext _dataContext;
+
+    public LikesRepository(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    public async Task<UserLike> GetUserLike(int sourceUserId, int likedUserId)
+    {
+        return await _dataContext.Likes.FindAsync(sourceUserId, likedUserId);
+    }
+
+    public async Task<AppUser> GetUserWithLikes(int userId)
+    {
+        return await _dataContext.Users
+            .Include(x => x.LikedUsers)
+            .FirstOrDefaultAsync(x => x.Id == userId);
+    }
+
+    public async Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams)
+    {
+        var users = _dataContext.Users.OrderBy(u => u.UserName).AsQueryable();
+        var likes = _dataContext.Likes.AsQueryable();
+
+        if (likesParams.Predicate == "liked")
+        {
+            likes = likes.Where(like => like.SourceUserId == likesParams.UserId);
+            users = likes.Select(like => like.LikedUser);
+        }
+
+        if (likesParams.Predicate == "likedBy")
+        {
+            likes = likes.Where(like => like.LikedUserId == likesParams.UserId);
+            users = likes.Select(like => like.SourceUser);
+        }
+
+        var likedUsers = users.Select(user => new LikeDto
+        {
+            Username = user.UserName,
+            KnownAs = user.KnownAs,
+            Age = user.Dob.CalculateUserAge(),
+            PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain).ImageUrl,
+            City = user.City,
+            Id = user.Id
+        });
+
+        return await PagedList<LikeDto>.CreateAsync(likedUsers, likesParams.PageNumber, likesParams.PageSize);
+    }
+}
--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../_models/member';
+import { Pagination } from '../_models/pagination';
+import { MemberService } from '../_services/member.service';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.css']
+  styleUrls: ['./lists.component.css'],
 })
 export class ListsComponent implements OnInit {
+  members: Partial<Member[]>;
+  predicate = 'liked';
+  pageNumber = 1;
+  pageSize = 5;
+  pagination: Pagination;
 
-  constructor() { }
+  constructor(private memberService: MemberService) {}
 
   ngOnInit(): void {
+    this.loadLikes();
   }
 
+  loadLikes() {
+    this.memberService
+      .getLikes(this.predicate, this.pageNumber, this.pageSize)
+      .subscribe((res) => {
+        this.members = res.result;
+        this.pagination = res.pagination;
+      });
+  }
+
+  pageChanged(event: any) {
+    this.pageNumber = event.page;
+    this.loadLikes();
+  }
 }
